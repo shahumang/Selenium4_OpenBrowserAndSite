4de211fb91cb9f6f01174f194c8f297860bec0b0
ushah <Umang.Shah@gantnerindia.com>
2023-04-29 18:01:14 +0530

    Added comments for understanding code

--- src/test/java/utility/Baseclass.java
+++ src/test/java/utility/Baseclass.java
@@ -10,22 +10,21 @@ import org.testng.annotations.BeforeSuite;
 import java.time.Duration;
 
 public class Baseclass {
-
-    //WebDriver is Interface so can't create it's object creating it's reference
+    //WebDriver is Interface so can't create it's object creating it's variable reference or instance
     public static WebDriver webDriver;
     @BeforeSuite
     public void openBrowser(){
 
         //Creating object of ChromeOptions class for using different methods of chrome
         ChromeOptions options = new ChromeOptions();
-
         //Maximize Chrome driver
         options.addArguments("--start-maximized");
         //Open Browser in incognito more
         options.addArguments("--incognito");
 
-        //Passing chromedriver as reference and default it's creating constructor and passing object of chromedriver
+        //Passing webDrive instance or reference variable for implementing chromedriver class and using runtime polymorephism
         webDriver = new ChromeDriver(options);
+        //implementing methods of WebDrive interface for opening browser
         webDriver.get("https://www.google.com/");
         webDriver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3));
     }
